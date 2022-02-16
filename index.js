83e7ad49cd1c79e12b8c00d532e0286e97dbec9a
imsahil2001 <sahilkkr56@gmail.com>
2022-02-16 14:32:49 +0530

    some design changes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let hours = new Date().getHours();
 let seconds = new Date().getSeconds();
 let daytime = "AM";
 let day = new Date().getDay();
+let day_time = document.getElementById("day_time");
+let dayComp = document.getElementById("day");
 
 let a = Array.from(document.querySelectorAll(".daytag"));
 a[day - 1].style.backgroundColor = "rgb(55, 55, 55)";
@@ -31,7 +33,7 @@ setInterval(() => {
   min = min < 10 ? "0" + min : min;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  dateHeading.innerHTML = `${hours}:${min}:${seconds} ${daytime} ${
-    daysArray[day - 1]
-  }`;
+  dateHeading.innerHTML = `${hours}:${min}:${seconds} `;
+  date_time.innerHTML = daytime;
+  dayComp.innerHTML = daysArray[day - 1];
 }, 1000);
